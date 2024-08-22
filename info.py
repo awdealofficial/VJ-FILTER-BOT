a22031d2ea1faf6992a9b1c9051d1bbd7a570833
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-08-22 23:33:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hindi_movie_up')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Hollywood_in_HindiHD')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'hindi_movie_up) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'hindi_movie_up') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
