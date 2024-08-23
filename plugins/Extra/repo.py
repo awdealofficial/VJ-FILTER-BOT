2f2c28515b0c461c105df91bd6cf398419b19298
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-08-23 12:53:49 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @Hollywood_in_HindiHD
 
 """
         if qw.get("description"):
