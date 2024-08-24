6d02cff6f19ee477e698ac957a1c40b0b7e4881d
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-08-24 11:58:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -703,13 +703,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://https://t.me/hollywood_in_hindihd'>Movie Updates</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://https://t.me/hollywood_in_hindihd'>Movie Updates</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://https://t.me/hollywood_in_hindihd>Movie Updates</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://https://t.me/hollywood_in_hindihd'>Movie Updates</a>
 """
 
     RENAME_TXT = """
